6a726c81e615c7ebc54bf44a3ce0b3022b483792
Thiago <thiagocezar32@gmail.com>
2022-07-18 12:43:33 -0300

    fds

---
+++ script.js
@@ -0,0 +1,33 @@
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
+for (let i = 1; i < 0; i++) {
+    console.log("tome")
+}
